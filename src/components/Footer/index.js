0af271882e13102116b63cce7d932b7d08acc1d3
hitesh-bhardwaj <70434060+hitesh-bhardwaj@users.noreply.github.com>
2024-02-19 17:02:40 +0530

    Update Final for all animations

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -14,17 +14,17 @@ const Footer = () => {
                         <div className="footer-top">
     
                         <div className="footer-top-left">
-                            <img className="footer-logo" loading='lazy' src="/logo-white.svg" alt="patronum logo" title="patronum logo"/>
-                            <p>Your Google Workspace Manager Providing a better way to manage your Google Workspace.</p>
+                            <img className="footer-logo fadeUp" loading='lazy' src="/logo-white.svg" alt="patronum logo" title="patronum logo"/>
+                            <p className="fadeUp">Your Google Workspace Manager Providing a better way to manage your Google Workspace.</p>
 
-                            <div className='flex gap-6 footer-iso'>
+                            <div className='flex gap-6 footer-iso fadeUp'>
                                 <img src='/assets/footer/ISO-1.svg' loading='lazy' alt='ISO Certficates'/>
                                 <img src='/assets/footer/ISO-2.svg' loading='lazy' alt='ISO Certficates'/>
                                 <img src='/assets/footer/ISO-3.svg' loading='lazy' alt='ISO Certficates'/>
                                 <img src='/assets/footer/ISO-4.svg' loading='lazy' alt='ISO Certficates'/>
                             </div>
 
-                            <div className='flex gap-6 footer-award'>
+                            <div className='flex gap-6 footer-award fadeUp'>
                                 <img loading='lazy' src="/assets/footer/award-2.png" alt="Award for best ease of use image" title="Award for best ease of use image"/>
                                 <img loading='lazy' src="/assets/footer/award-1.png" alt="Award for best value image" title="Award for best value image"/>
                                 <img loading='lazy' src="/assets/footer/google-partner-white.svg" alt="Patronum Google Patrner" title="Patronum Google Patrner"/>
@@ -34,8 +34,8 @@ const Footer = () => {
                         <div className='footer-top-right'>
                             <div className='footer-top-right-top'>
                                 <div className="">
-                                    <h6 className="footer-link-head">Important Links</h6>
-                                    <ul className='footer-links'>
+                                    <h6 className="footer-link-head fadeUp">Important Links</h6>
+                                    <ul className='footer-links fadeUp'>
                                         <li className="py-2">
                                             <a href="/features" className="text-xl">
                                                 Features
@@ -70,8 +70,8 @@ const Footer = () => {
                                 </div>
 
                                 <div className="">
-                                    <h6 className="footer-link-head">Other Links</h6>
-                                    <ul className='footer-links'>
+                                    <h6 className="footer-link-head fadeUp">Other Links</h6>
+                                    <ul className='footer-links fadeUp'>
                                         <li className="py-2">
                                             <a href="/blog" className="text-xl">
                                                 About
@@ -101,7 +101,7 @@ const Footer = () => {
                                 </div>
                             </div>
 
-                            <div className='social-links'>
+                            <div className='social-links fadeUp'>
                                 <ul className=''>
                                     <li>
                                         <a href='#'>
@@ -134,13 +134,13 @@ const Footer = () => {
                         </div>
                 </div>
 
-                <div className="py-10 px-14">
-                    <p className="text-center font-[400] text-[15px]">Bespin Labs Limited is registered with the Information Commissioners Office in relation to the processing of personal information (registration number: ZA686937). Bespin Labs Limited is incorporated in England and Wales (company number: 11068628). Our registered office is First Floor Office Suite, Mill B Colne Road Buildings, Colne Road, Huddersfield, United Kingdom, HD1 3AG</p>
+                <div className="py-10 px-14 scaleAnim">
+                    <p className="text-center font-[400] text-[0.8vw]">Bespin Labs Limited is registered with the Information Commissioners Office in relation to the processing of personal information (registration number: ZA686937). Bespin Labs Limited is incorporated in England and Wales (company number: 11068628). Our registered office is First Floor Office Suite, Mill B Colne Road Buildings, Colne Road, Huddersfield, United Kingdom, HD1 3AG</p>
                 </div>
 
-                <div className="py-6 border-t border-gray-200 text-[16px] flex justify-between text-center text-[aeonik]">
+                <div className="py-6 text-[0.8vw] flex justify-between text-center text-[aeonik] tracking-[0.5px]">
                     <p className="">© 2024 <a href="www.bespinlabs.com" target="black" className="">Bespin Labs Ltd.</a> All rights reserved.</p>
-                    <a href='#'>By : Enigma</a>
+                    <a href='https://weareenigma.com' title="We Create Magic">By: <span className="relative after:absolute after:bg-white after:h-[1px] after:left-0 after:bottom-[-2px] after:scale-0 hover:after:scale-100 after:duration-300 after:w-full">Enigma</span></a>
                 </div>
                 </div>
             </div>
