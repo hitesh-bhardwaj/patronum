12a63c045aca04b439ad9cba86ea1f41a0f179cb
hitesh-bhardwaj <70434060+hitesh-bhardwaj@users.noreply.github.com>
2024-04-02 14:11:51 +0530

    Update Before MailChimp

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -169,7 +169,7 @@ export default function App({ Component, pageProps, router }) {
       </ReactLenis>
       <SpeedInsights />
       <Analytics />
-      {/* {loadBackground && <Background />} */}
+      {loadBackground && <Background />}
     </>
   ); 
 }
