e3dceb41b47f7967176f4e3951fff7d29123207d
Hitesh Bhardwaj <bhardwajhitesh09@gmail.com>
2024-11-18 16:31:00 +0530

    Update - Optimizations
    Blog Page LCP

--- src/components/PageComponents/BlogPage/BlogLayout.js
+++ src/components/PageComponents/BlogPage/BlogLayout.js
@@ -9,6 +9,7 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import BreadcrumbComponent from "@/components/PageLayout/BreadCrumb";
 import Layout from "@/components/Layout";
+import Image from "next/image";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -130,8 +131,9 @@ export default function BlogLayout({
                       </div>
                   </div>
                 </div>
-                <div className="featured-image overflow-hidden lg:rounded-3xl rounded-2xl lg:h-[35vw] h-[full] fadeUp">
-                  <img
+                <div className="featured-image relative overflow-hidden lg:rounded-3xl rounded-2xl lg:h-[35vw] w-full md:h-[45vw] h-[50vw] fadeUp">
+                  <Image
+                    fill
                     className="w-full h-full object-cover object-center"
                     src={featImg}
                     alt={`${postTitle} image`}
